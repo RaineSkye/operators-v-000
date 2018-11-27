2144753303c896093de1f2382bf4183ad616765c
kitte anderson <kitteanderson@gmail.com>
2018-11-27 01:59:32 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,7 @@
-require 'pry'
+
 def unsafe?(speed)
 	if speed < 40 || speed > 60
 		true
-		binding.pry
 	else
 		false
 	end
@@ -12,5 +11,4 @@ end
 
 def not_safe?(speed)
 	speed > 60 || speed < 40 ? true : false
-	binding.pry
 end
